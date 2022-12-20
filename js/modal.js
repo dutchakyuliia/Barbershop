9ed8373839696304991224c2f809eb7c1558def7
Dutchak Yuliia <dutchakyuliia@gmail.com>
2022-12-20 16:40:36 +0200

    Make open modal for each button online-booking

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,17 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+
   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function openModal(item) {item.addEventListener('click', toggleModal)};
+  refs.openModalBtn.forEach(openModal);
+  
+
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
